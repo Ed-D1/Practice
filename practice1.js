3685957575eae764a2fd0eff63bb4a71ebcd1598
Ed-d1 <eddie943@outlook.com>
2019-11-10 18:56:24 -0600

    circular motion with perlin noise

--- practice1.js
+++ practice1.js
@@ -1,18 +1,25 @@
-
+let phase = 0;
+let zoff = 0;
+let noiseMax = 10;
 function setup(){
-  createCanvas(600, 600);
+  createCanvas(windowWidth, windowHeight);
 }
 function draw(){
-  background(0);
+  background(0, 50);
   translate(width/2, height/2);
   stroke(255);
+  strokeWeight(2);
   noFill();
   beginShape();
   for (let a = 0; a <TWO_PI; a += 0.01){
-    let r = random(50,100);
-    let x = r * cos(a);
-    let y = r * sin(a);
-    vertex(x,y);
+    let xoff = map(sin(a + phase), -1, 0, 1, noiseMax);
+    let yoff = map(cos(a +phase), -1, 0, 1, noiseMax);
+    let r = map(noise(xoff, yoff), 0, 1, 100, height/2);
+    let x = -r * sin(a);
+    let y = r * cos(a);
+    vertex(y,x);
   }
-  endShape();
+  endShape(CLOSE);
+phase += 0.1;
+zoff += 0.1;
 }
